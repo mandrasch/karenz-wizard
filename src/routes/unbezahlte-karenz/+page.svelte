9e8862a630c99c74e5a6301b8b1f3f4e6fe52f39
Matthias Andrasch <777278+mandrasch@users.noreply.github.com>
2025-12-03 23:34:37 +0100

    chore: add own page for unbezahlte karenz, fix link

--- src/routes/unbezahlte-karenz/+page.svelte
+++ src/routes/unbezahlte-karenz/+page.svelte
@@ -1,10 +1,160 @@
-<!-- TODO: add info about presseanfrage ögk, AK meldung -->
-
 <section class="content">
-	<header class="mt-10">
+	<header class="mt-10 space-y-4 text-center">
 		<h1 class="text-3xl font-semibold text-slate-900">Unbezahlte Karenz</h1>
 	</header>
-	<div class="prose mt-6">
-		<p class="font-bold">🚧 Inhalt in Arbeit 🚧</p>
+
+	<div class="mx-auto prose mt-6 max-w-4xl space-y-10">
+		<p>🚧 Inhalt in Arbeit 🚧</p>
+		<!-- TODO: definition adden -->
+		<p>
+			Bei einer unbezahlten Karenz stellen sich primär die Frage: Wie bin ich krankenversichert?
+		</p>
+
+		<!-- TODO: Man lässt sich beim Partner mitversichern -->
+
+		<strong
+			>Die Krankenversicherung in der unbezahlten Karenz: Sich im besten Fall beim Partner/in in der
+			KV mitversichern lassen</strong
+		>
+
+		<p class="has-vivid-red-color has-text-color has-link-color">
+			⚠️ Ich selbst habe bisher keine unbezahlte Karenz in Anspruch genommen. Alle Angaben ohne
+			Gewähr, bitte bei der Arbeiterkammer und/oder ÖGK vorher beraten lassen! Sollte die
+			Mitversicherung abgelehnt werden, können schlimmstenfalls bis zu 526,79€ / Monat für
+			<a
+				href="https://www.gesundheitskasse.at/cdscontent/?contentid=10007.867315&amp;portal=oegkportal"
+				>freiwillige Selbstversicherung</a
+			> nötig sein - falls die Herabsetzung am Ende auch nicht gewährt wird von der ÖGK, siehe unten.
+			Das ist das Risiko. ⚠️
+		</p>
+
+		<h3>Verheiratete Paare</h3>
+		<strong>Bei verheirateten Paaren:</strong> Hier reicht es anscheinend (!) laut ÖGK-Webseite, die
+		normalen Anforderungen zu erfüllen. Kein Nachweis der Haushaltsführung.
+
+		<figure>
+			<img
+				src="https://workwhileclimate.at/wp-content/uploads/2025/08/image-2-1024x675.jpg"
+				alt="Screenshot"
+			/>
+			<figcaption>
+				Screenshot ÖGK-Webseite,
+				<a
+					href="https://www.gesundheitskasse.at/cdscontent/?contentid=10007.870665&amp;portal=oegkportal"
+					>Mitversicherung Partner*in - Vorraussetzungen</a
+				> (Stand: 13.08.2025), Rechte: ÖGK
+			</figcaption>
+		</figure>
+
+		<h3>Unverheiratete Paare: Prüfformular Haushaltsführung</h3>
+		<p>
+			Die aktuelle Lage ist leider unübersichtlich für unverheiratete Paare. Diese müssen bei der
+			ÖGK inzwischen eine "unbezahlte Haushaltsführung seit mindestens 10 Monaten" nachweisen wenn
+			ein Partner:in während der unbezahlten Karenz mitversichert werden soll. Dieser Nachweis kann
+			wohl ggf. ein Falstrick in der Praxis sein, zumindest laut kurzer Info der AK-Beratung.
+		</p>
+		<strong>Bei unverheirateten Paaren:</strong> Eine Mitversicherung beim Lebensgefährten/in ist
+		nur möglich ist, wenn man seit mindestens 10 Monaten zusammenwohnt. Es darf außerdem kein
+		Ehegatte oder eingetragener Partner im Haushalt leben.
+		<strong
+			>⚠️ UND: Der/die Partner:in, die mitversichert werden will, muss seit mindestens 10 Monaten
+			unbezahlt den Haushalt führen.</strong
+		>
+
+		<p>
+			Die ÖGK hat hier einige Antworten bereitgestellt:
+			<a href="/blog/antwort-presseanfrage-oegk-unbezahlte-karenz-mitversicherung/"
+				>Antwort der ÖGK zur Anfrage "Unbezahlte Karenz und Mitversicherung - haushaltsführende
+				Person" (unverheiratete Paare)</a
+			>
+		</p>
+
+		<p>
+			Siehe auch Reddit-Diskussion:
+			<a
+				href="https://www.reddit.com/r/Austria/comments/1mqnclb/%C3%B6gk_mitversicherung_partnerin_unverheiratet_was/"
+				>ÖGK Mitversicherung Partner*in (unverheiratet)</a
+			>
+		</p>
+
+		<p>
+			Sollte eine Mitversicherung nicht möglich sein, kann man sich selbst bei der ÖGK die
+			freiwillige Selbstversicherung (526,79€ / Monat) nutzen, aber direkt die Herabsetzung der
+			Beiträge beantragen:
+			<a
+				href="https://www.gesundheitskasse.at/cdscontent/?contentid=10007.867315&amp;portal=oegkportal"
+				>Selbstversicherung in der Krankenversicherung - Herabsetzung des Beitrages beantragen</a
+			>
+			(<em>🚧 Wie viel wäre das dann am Ende? 🚧</em>)
+		</p>
+
+		<p>Kostet die Mitversicherung etwas? 🚧 In Arbeit 🚧</p>
+
+		<blockquote>
+			<p>
+				Ich nehme zur Kenntnis, dass gemäß § 51d ASVG für bestimmte Angehörige monatlich ein
+				Krankenversicherungsbeitrag von 3,4 % meiner Beitragsgrundlage zu entrichten ist
+				(gegebenenfalls rückwirkend bis zu fünf Jahre).
+			</p>
+		</blockquote>
+
+		<p>
+			Siehe auch Reddit Frage:
+			<a
+				href="https://www.reddit.com/r/FinanzenAT/comments/1mkydzk/ea_kinderbetreuungsgeld_und_danach_unbezahlte/"
+				>ea Kinderbetreuungsgeld und danach unbezahlte Karenz - Kranken- & Pensionsversicherung?</a
+			>
+		</p>
+
+		<p><strong>Was sind Alternativen zur unbezahlten Karenz?</strong></p>
+
+		<ol>
+			<li>
+				Wer lieber komplett auf Nummer sicher gehen will, kann das Pauschale
+				Kinderbetreuungsgeld-Konto nutzen. Dieses ermöglicht eine längere Karenz-Zeit als nur 14
+				Monate (ab Geburt) beim einkommensabhängigen Kinderbetreuungsgeld. Beim Pauschalen KBG ist
+				also keine unbezahlte Karenz nötig, man ist krankenversichert — aber es kommt eben meist
+				weniger finanzielle Förderung dabei heraus im Vergleich. Ein bisschen den Verlust auffangen
+				kann man hier ggf. noch durch den einmaligen 1.000€-Partnerschaftsbonus bei 50:50 bis zu
+				60:40 Aufteilung.
+			</li>
+			<li>
+				Beide Elternteile gehen in <a href="https://www.arbeiterkammer.at/elternteilzeit"
+					>Eltern-Teilzeit</a
+				>
+				im Anschluss an die 14 Monate einkommensabhängigem Kinderbetreuungsgeld (gezählt ab Geburt).
+				Beispielsweise beide arbeiten 20h die Woche, einer vormittags und einer nachmittags — dies kann
+				sich ggf. auch steuerlich lohnen (?).
+				<ul>
+					<li>
+						Good to know: Bei einer Eltern-Teilzeit kann entweder die Arbeitszeit um mindestens 20%
+						reduziert werden — oder Eltern können nur die "die Lage Ihrer Arbeitszeiten so
+						verschieben, dass Sie Beruf und Familie leichter unter einen Hut bekommen." (<a
+							href="https://www.arbeiterkammer.at/elternteilzeit">AK - Elternteilzeit</a
+						>, Anspruch vorab prüfen!)
+					</li>
+				</ul>
+			</li>
+			<li>Tagesmutter/Vater mit kleinerer Gruppen-Größe statt Kindergarten</li>
+			<li>Support von Großeltern o.ä.</li>
+			<li>
+				Kindergarten-Eingewöhnung, Obacht: nicht in jedem Bundesland unter 2 Jahren wie in Wien
+				möglich
+				<ul>
+					<li>
+						Privater Erfahrungsbericht Wien: „Wien ist mit 12 oder 14 Monaten kein Problem.
+						Kleinkinder Gruppen haben einen Betreuungsschlüssel von 15:3 wobei bei uns selten
+						tatsächlich mehr wie 10 Kinder gleichzeitig anwesend sind, das ist im Vergleich zu den
+						normalen Kindergartengruppen (25:2) wesentlich besser und (außer Essen) kostenlos. Wir
+						waren damit völlig zufrieden und machen es bei K2 wieder so. Allerdings könnte ich es
+						mir in dem Alter nicht von 8 bis 17 Uhr vorstellen, aber 8 bis 12 Uhr in Kombination mit
+						Teilzeit (insbesondere wenn man den Job mag) eben schon.“ (<a
+							href="https://www.reddit.com/r/FinanzenAT/comments/1mkydzk/comment/n8iaxyk/">Reddit</a
+						>)
+					</li>
+					<!-- TODO: FAQ - ab wann Fremdbetreuung -->
+				</ul>
+			</li>
+		</ol>
 	</div>
 </section>
